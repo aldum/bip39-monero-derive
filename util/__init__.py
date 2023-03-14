7e261e2e9c7307ba09cbff92cb8f9a737ddc3d36
aldum <aldum@artixlinux.org>
2023-03-15 00:14:28 +0100

    refactor: clean utils

--- util/__init__.py
+++ util/__init__.py
@@ -1,26 +1,6 @@
 """Utility functions"""
 
 
-def b(s):
-    # utf-8 would cause some side-effects we don't want
-    return s.encode("latin-1")
-
-
-def bchr(s):
-    return bytes([s])
-
-
-def bstr(s):
-    if isinstance(s, str):
-        return bytes(s, "latin-1")
-    else:
-        return bytes(s)
-
-
-def bord(s):
-    return s
-
-
 def tobytes(s, encoding="latin-1"):
     if isinstance(s, bytes):
         return s
@@ -34,12 +14,8 @@ def tobytes(s, encoding="latin-1"):
         return bytes([s])
 
 
-def tostr(bs) -> str:
-    return bs.decode("latin-1")
 
 
-def byte_string(s) -> bool:
-    return isinstance(s, bytes)
 
 
 def strxor(s1: bytes, s2: bytes) -> bytes:
