01b157f0cfb833ede5fc36c3b2d01d477f68aeae
aldum <aldum@artixlinux.org>
2023-03-14 21:10:12 +0100

    test: setup tox

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,13 +2,13 @@ import unittest
 
 import Crypto.Util.strxor as ref
 
-from __init__ import (strxor, tobytes)  # disable=E0611
+from util import (strxor, tobytes)  # disable=E0611
 
 
 class TestUtils(unittest.TestCase):
     def test_strxor(self):
-        b1 = b'0x01x02'
-        b2 = b'0x03x04'
+        b1 = b'\x01\x02'
+        b2 = b'\x03\x04'
         self.assertEqual(strxor(b1, b2), ref.strxor(b1, b2))
 
 
