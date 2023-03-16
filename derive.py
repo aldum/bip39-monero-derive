b7a2ee2dda63c2910232369e4b4ba33abe15533e
aldum <aldum@artixlinux.org>
2023-03-16 21:02:05 +0100

    feat: catch SIGINT

--- derive.py
+++ derive.py
@@ -1,14 +1,17 @@
 """Interactive application for deriving a Monero mnemonic from a BIP 39 seedphrase."""
-
+import signal
 from curses import wrapper
 import ui
 
 
 def __main__():
-    try:
-        wrapper(ui.program)
-    except KeyboardInterrupt:
-        print("Bye.")
+    def handler(_sig, _handler):
+        print("Ctrl+C pressed.")
+
+    # Set the signal handler
+    signal.signal(signal.SIGINT, handler)
+
+    wrapper(ui.program)
 
 
 __main__()
