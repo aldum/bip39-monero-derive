bbabac4847be575d756732e8e0c04598b85dc42d
aldum <aldum@artixlinux.org>
2023-03-20 21:41:43 +0100

    test: clean integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,7 +6,6 @@ from binascii import hexlify
 
 from util import err_print
 from tests.util import JSONUtils
-from bip39 import mnemonics_to_ent  # , mnem_to_binstr
 from slip0010 import sd
 
 
@@ -20,17 +19,14 @@ class TestUtils(TestCase):
 
     def test_integration(self):
         for l in self.test_data:
-            ent = l.seed
             ent = l.entropy
-            gen_ent = mnemonics_to_ent(l.bip39, l.passp)
             mnem = l.monero_mnem
-            # err_print(f"b58 {hexlify(gen_ent)}")
-            assert ent == gen_ent
+            der = sd.SeedDerivation.derive_monero(l.bip39, l.passp)
+            err_print(
+                f"sd {hexlify(ent)}\n   {hexlify(der.master_seed)}")
+            assert der.master_seed == ent
             if mnem is not None:
-                der = sd.SeedDerivation.derive_monero(l.bip39, l.passp)
-                assert der.master_seed == gen_ent
-                asdf = der.monero_master
-                err_print(f"mm {hexlify(asdf)}")
+                # assert der.master_seed == gen_ent
                 mnem_words = der.electrum_words
                 assert mnem_words.split(' ') == mnem.split(' ')
         # assert 1 == 2
