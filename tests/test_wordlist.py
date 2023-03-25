a80826676f68d3727c6f6057da54e75b62f513c9
aldum <aldum@artixlinux.org>
2023-03-25 10:17:42 +0100

    feat: add unique prefix dict to wordlist

---
+++ tests/test_wordlist.py
@@ -0,0 +1,20 @@
+import unittest
+
+from bip39 import wordlist
+# from util import err_print
+
+
+class TestUtils(unittest.TestCase):
+    def test_wordlist(self):
+        pl = wordlist.unique_prefix_length
+        # for i in range(5):
+        for i in range(wordlist.WORDS_LIST_NUM):
+            word = wordlist[i]
+            pref = word[:pl]
+            # err_print(f"{pref} {wordlist[i]}")
+            assert wordlist.unique_prefixes[pref] == word
+        # assert 1 == 2
+
+
+if __name__ == '__main__':
+    unittest.main()
--- util/wordlist.py
+++ util/wordlist.py
@@ -17,13 +17,20 @@ class Wordlist:
     WORDS_LIST_NUM: int = 0
     MAX_WORDLEN = 0
     m_words_to_idx = {}
+    unique_prefix_length = 0
+    unique_prefixes = {}
 
     def __init__(self):
-        self.WORDS_LIST_NUM = len(self.wordlist)
+        n = len(self.wordlist)
+        self.WORDS_LIST_NUM = n
         self.MAX_WORDLEN = reduce(
             lambda a, s: max(a, len(s)), self.wordlist, 0)
         self.m_words_to_idx = {
-            self.wordlist[i]: i for i in range(len(self.wordlist))}
+            self.wordlist[i]: i for i in range(n)}
+        self.unique_prefixes = {
+            self.wordlist[i][:self.unique_prefix_length]:
+                self.wordlist[i] for i in range(n)
+        }
 
     def __getitem__(self, key):
         return self.wordlist[key]
