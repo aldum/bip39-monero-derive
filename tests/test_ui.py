e25e94f8b6214e35d9fa3dbc08d7b6d40b6084e6
aldum <aldum@artixlinux.org>
2023-05-27 20:09:05 +0200

    test: add ui test poc

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,18 +1,69 @@
+import os
 import curses
+import select
+import unittest
+from typing import Any, Callable
 
-from ui import wait
-
-
-# def test_getcoords(screen):
-#     screen.clear()
-#     screen.clear()
-#     screen.addstr("\n")
-#     screen.addstr("\n")
-#     (x, y) = screen.getyx()
-#     (mx, my) = screen.getmaxyx()
-#     screen.addstr("\n" + f"coords: {x} {y}")
-#     screen.addstr("\n" + f"size: {mx} {my}")
-#     wait(screen)
+from ui import program
+import pyte # type: ignore
 
 
-# curses.wrapper(test_getcoords)
+class TestUI(unittest.TestCase):
+
+    @staticmethod
+    def _test_wrapper(test: Callable[[Any], Any]) -> None:
+        pid, f_d = os.forkpty()
+        if pid == 0:
+            curses.wrapper(program)
+        else:
+            test(f_d)
+
+    def test_bounds_check(self):
+        def test(f_d):
+            # screen = pyte.Screen(10, 20)
+            self.assertRaises(AssertionError)
+        self._test_wrapper(test)
+
+    def test_tui(self):
+        def test(f_d) -> None:
+            screen = pyte.Screen(80, 25)
+            stream = pyte.ByteStream(screen)
+
+            os.write(f_d, str.encode("\n"))
+            for i in range(11):
+                os.write(f_d, str.encode("aban\n"))
+            os.write(f_d, str.encode("abou\n"))
+            os.write(f_d, str.encode("j")) # picker down
+            os.write(f_d, str.encode("\n"))
+            while True:
+                try:
+                    [f_d], _, _ = select.select(
+                        [f_d], [], [], 1)
+                except (KeyboardInterrupt, ValueError):
+                    # either test was interrupted or the
+                    # file descriptor of the child process
+                    # provides nothing to be read
+                    break
+                else:
+                    try:
+                        # scrape screen of child process
+                        data = os.read(f_d, 1024)
+                        stream.feed(data)
+                    except OSError:
+                        # reading empty
+                        break
+            # for line in screen.display[11]:
+            #     print(line)
+            l1: str = screen.display[11]
+            l2: str = screen.display[12]
+            part1 = \
+                "alchemy apart fowls dexterity puck films liquid vigilant yesterday people awful"
+            part2 = "blender plywood"
+            self.assertEqual(l1.strip(), part1)
+            self.assertEqual(l2.strip(), part2)
+
+        self._test_wrapper(test)
+
+
+if __name__ == '__main__':
+    unittest.main()
