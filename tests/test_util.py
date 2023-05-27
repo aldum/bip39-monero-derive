a3b14990f1c42d7502075cdba981c5ef194fef7c
aldum <aldum@artixlinux.org>
2023-05-27 22:02:42 +0200

    test: add bstr-int tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,10 @@
 import unittest
 
 import Crypto.Util.strxor as ref # type: ignore
+from hypothesis import assume, given
+from hypothesis import strategies as st
 
-from util import strxor
+from util import strxor, IntegerUtils as I
 
 
 class TestUtils(unittest.TestCase):
@@ -10,6 +12,29 @@ class TestUtils(unittest.TestCase):
         b1 = b'\x01\x02'
         b2 = b'\x03\x04'
         self.assertEqual(strxor(b1, b2), ref.strxor(b1, b2))
+
+class TestIntegerUtils(unittest.TestCase):
+    def test_to_bstr(self):
+        self.assertEqual(I.to_binary_str(0), '0')
+        self.assertEqual(I.to_binary_str(0, 2), '00')
+        self.assertEqual(I.to_binary_str(1), '1')
+        self.assertEqual(I.to_binary_str(1, 2), '01')
+
+    def test_from_bstr(self):
+        self.assertEqual(I.from_binary_str('0'), 0)
+        self.assertEqual(I.from_binary_str('1'), 1)
+
+    @given(st.integers(min_value=0))
+    def test_from_inverts_to(self, i):
+        print(i)
+        assert I.from_binary_str(I.to_binary_str(i)) == i
+
+    @given(st.lists(st.sampled_from(['0','1'])))
+    def test_to_inverts_from(self, bl):
+        bs = ''.join(bl)
+        assume(bs) # filter empties
+        # converting to int to account for leading zeros
+        assert int(I.to_binary_str(I.from_binary_str(bs)), 2) == int(bs, 2)
 
 
 if __name__ == '__main__':
