7e674f8010e45b2de7e34dc3f085f40a61c213b2
aldum <aldum@artixlinux.org>
2023-05-28 21:00:30 +0200

    test: add prop testing for utils

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,9 +1,17 @@
 import unittest
 
+from hypothesis import assume, given
+from hypothesis import strategies as st
+
 import Crypto.Util.strxor as ref # type: ignore
 
-from util import strxor
+from util import (
+    strxor,
+    IntegerUtils as I,
+    BytesUtils as B,
+)
 
+blists = st.lists(st.sampled_from(['0','1']))
 
 class TestUtils(unittest.TestCase):
     def test_strxor(self):
@@ -11,6 +19,46 @@ class TestUtils(unittest.TestCase):
         b2 = b'\x03\x04'
         self.assertEqual(strxor(b1, b2), ref.strxor(b1, b2))
 
+class TestIntegerUtils(unittest.TestCase):
+    def test_to_bstr(self):
+        self.assertEqual(I.to_binary_str(0), '0')
+        self.assertEqual(I.to_binary_str(0, 2), '00')
+        self.assertEqual(I.to_binary_str(1), '1')
+        self.assertEqual(I.to_binary_str(1, 2), '01')
+
+    def test_from_bstr(self):
+        self.assertEqual(I.from_binary_str('0'), 0)
+        self.assertEqual(I.from_binary_str('1'), 1)
+
+    @given(st.integers(min_value=0))
+    def test_from_inverts_to(self, i):
+        print(i)
+        assert I.from_binary_str(I.to_binary_str(i)) == i
+
+    @given(blists)
+    def test_to_inverts_from(self, bl):
+        bs = ''.join(bl)
+        assume(bs) # filter empties
+        # converting to int to account for leading zeros
+        self.assertEqual(
+            int(I.to_binary_str(I.from_binary_str(bs)), 2),
+            int(bs, 2)
+        )
+
+class TestBytesUtils(unittest.TestCase):
+    def test_to_bstr(self):
+        self.assertEqual(
+            B.to_binary_str( (0).to_bytes(1, byteorder='big') ),
+            '0'
+        )
+        self.assertEqual(
+            B.to_binary_str( (1).to_bytes(1, byteorder='big') ),
+            '1'
+        )
+        self.assertEqual(
+            B.to_binary_str( (4).to_bytes(1, byteorder='big') ),
+            '100'
+        )
 
 if __name__ == '__main__':
     unittest.main()
