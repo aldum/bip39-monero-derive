860268b71c55f408042878064b44195bf6fef9ea
aldum <aldum@artixlinux.org>
2023-05-30 09:21:21 +0200

    test: add generated strxor tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -12,11 +12,14 @@ from util import (
 )
 
 blists = st.lists(st.sampled_from(['0','1']))
+two_binaries = st.integers(min_value=0, max_value=10).flatmap(
+    lambda i: st.tuples(st.binary(min_size=i, max_size=i), st.binary(min_size=i, max_size=i))
+)
 
 class TestUtils(unittest.TestCase):
-    def test_strxor(self):
-        b1 = b'\x01\x02'
-        b2 = b'\x03\x04'
+    @given(two_binaries)
+    def test_strxor(self, tup):
+        (b1, b2) = tup
         self.assertEqual(strxor(b1, b2), ref.strxor(b1, b2))
 
 class TestIntegerUtils(unittest.TestCase):
